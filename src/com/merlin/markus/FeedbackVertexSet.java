5e0bb043b1962a60ecc479b086bd0ede6bba27a7
Markus-PC\Markus <markus.brand@student.hpi.de>
2019-11-24 15:00:22 +0100

    Bugfixing and start performance optimization

--- src/com/merlin/markus/FeedbackVertexSet.java
+++ src/com/merlin/markus/FeedbackVertexSet.java
@@ -12,6 +12,11 @@ public class FeedbackVertexSet {
     private HashMap<Integer, Set<Integer>> originalGraph;
 
     public FeedbackVertexSet(String fileUrl) {
+       /* Integer[] a = {1, 2, 3, 4};
+        Integer[] b = {1, 3, 2, 4};
+        ArrayList<Integer> lcs = this.LCS(new ArrayList<>(Arrays.asList(a)), new ArrayList<>(Arrays.asList(b)));
+        System.out.println(lcs.size());*/
+
         File file = new File(fileUrl);
         originalGraph = new HashMap<>();
         try {
@@ -41,29 +46,26 @@ public class FeedbackVertexSet {
         FeedbackVertexSet vertexCover = new FeedbackVertexSet(fileUrl);
         ArrayList<Integer> solution = vertexCover.solve();
         for (int node: solution) {
-            System.out.println(node);
+            System.out.println("Solution" + node);
         }
     }
+
 
     private ArrayList<Integer> solve() {
-        for (int k = 0; k < originalGraph.size(); k++) {
-            ArrayList<Integer> solution = solveWithK(k);
-            if(solution != null) return solution;
-        }
-        return null;
-    }
-
-    private ArrayList<Integer> solveWithK(int k) {
+        int k = 0;
         ArrayList<Integer> solution = new ArrayList<>();
-        for(int i = 1; i <= k + 1; i++) {
-            solution.add(i);
-        }
 
-        for(int graphSize = k ;graphSize < originalGraph.size() - 1; graphSize++) {
+        for(int graphSize = k; graphSize < originalGraph.size(); graphSize++) {
+            System.out.println("--------------- graphSize:" + graphSize);
             HashMap<Integer, Set<Integer>> kPlusOneGraph = getSubGraph(originalGraph, graphSize + 1);
-            solution = compress(kPlusOneGraph, solution, k);
-            if(solution == null || solution.size() > k) {
-                return null;
+            solution.add(graphSize + 1);
+            if(solution.size() <= k) continue;
+            ArrayList<Integer> newSolution = compress(kPlusOneGraph, solution, k);
+            if(newSolution == null) {
+                k++;
+                System.out.println("--------------- K:" + k);
+            } else {
+                solution = newSolution;
             }
         }
 
@@ -89,22 +91,92 @@ public class FeedbackVertexSet {
                 bitMask >>= 1;
             }
             HashMap<Integer, Set<Integer>> graphWithoutX = removeNodesFromGraph(kPlusOneGraph, X);
-            System.out.println(X.size() + " - " + Y.size() + " - " + graphWithoutX.size());
-            ArrayList<Integer> solution = solveDisjoint(graphWithoutX, Y, k - X.size() + 1);
+            ArrayList<Integer> solution = solveDisjoint(graphWithoutX, Y, Y.size() - 1);
             if(solution != null) {
+                solution.addAll(X);
+                System.out.println(X.size() + " - " + Y.size() + " - " + graphWithoutX.size() + " - " + kPlusOneGraph.size() + " - " + oldSolution.size() + " - " + solution.size());
                 return solution;
             }
         }
         return null;
     }
 
-    private ArrayList<Integer> solveDisjoint(HashMap<Integer, Set<Integer>> graphWithoutX, ArrayList<Integer> y, long l) {
+    private ArrayList<Integer> solveDisjoint(HashMap<Integer, Set<Integer>> graphWithoutX, ArrayList<Integer> y, long k) {
         ArrayList<Integer> totalOrderInY = getTotalOrder(graphWithoutX, y);
         if (totalOrderInY == null) return null;
         List<Integer> x = graphWithoutX.keySet().stream().filter(key -> !y.contains(key)).collect(Collectors.toList());
         ArrayList<Integer> totalOrderInX = getTotalOrder(graphWithoutX, new ArrayList<>(x));
         if (totalOrderInX == null) return null;
+
+        ArrayList<Integer> solution = new ArrayList<>();
+        for(int node : totalOrderInX) {
+            if(isCyclic(node, totalOrderInY, graphWithoutX)) {
+                solution.add(node);
+            }
+        }
+
+        if(solution.size() > k) return null;
+
+        totalOrderInX.removeAll(solution);
+
+        //Einsortieren
+        ArrayList<Integer> orderCopy = new ArrayList<>(totalOrderInX);
+        ArrayList<Integer> order2 = new ArrayList<>();
+
+        for(int yNode : totalOrderInY) {
+            for (int i = 0; i < orderCopy.size(); i++) {
+                int xNode = orderCopy.get(i);
+                if(graphWithoutX.get(xNode).contains(yNode)) {
+                    order2.add(xNode);
+                    orderCopy.remove(i);
+                    i--;
+                }
+            }
+        }
+        order2.addAll(orderCopy);
+        //LCS
+
+        ArrayList<Integer> lcs = LCS(new ArrayList<>(totalOrderInX), new ArrayList<>(order2));
+        for(int node : totalOrderInX) {
+            if(!lcs.contains(node)) {
+                solution.add(node);
+            }
+        }
+
+        System.out.println("post" + solution.size());
+        if(solution.size() <= k) {
+            return solution;
+        }
         return null;
+    }
+
+    private ArrayList<Integer> LCS(ArrayList<Integer> a, ArrayList<Integer> b) {
+        if(a.isEmpty() || b.isEmpty()) return new ArrayList<>();
+        int lastA = a.get(a.size() - 1);
+        int lastB = b.get(b.size() - 1);
+        if(lastA == lastB) {
+            a.remove(a.size() - 1);
+            b.remove(b.size() - 1);
+            ArrayList<Integer> solution = LCS(a, b);
+            solution.add(lastA);
+            return solution;
+        } else {
+            ArrayList<Integer> aCopy = new ArrayList<>(a);
+            a.remove(a.size() - 1);
+            ArrayList<Integer> bCopy = new ArrayList<>(b);
+            b.remove(b.size() - 1);
+
+            ArrayList<Integer> solution1 = LCS(aCopy, b);
+            ArrayList<Integer> solution2 = LCS(a, bCopy);
+            return solution1.size() > solution2.size() ? solution1 : solution2;
+        }
+    }
+
+    private boolean isCyclic(int node, ArrayList<Integer> totalOrderInY, HashMap<Integer, Set<Integer>> graphWithoutX) {
+        OptionalInt minOutgoingPosition = graphWithoutX.get(node).stream().map(totalOrderInY::indexOf).mapToInt((lol) -> lol).min();
+        OptionalInt maxIncomingPosition = totalOrderInY.stream().filter(n -> graphWithoutX.get(n).contains(node)).map(totalOrderInY::indexOf).mapToInt((lol) -> lol).max();
+        if(!minOutgoingPosition.isPresent() || !maxIncomingPosition.isPresent()) return false;
+        return maxIncomingPosition.getAsInt() > minOutgoingPosition.getAsInt();
     }
 
     private ArrayList<Integer> getTotalOrder(HashMap<Integer, Set<Integer>> graphWithoutX, ArrayList<Integer> y) {
@@ -119,7 +191,7 @@ public class FeedbackVertexSet {
             for(int neighbour : outDegreeY.keySet()){
                 if(outDegreeY.get(neighbour) == 0) {
                     foundSomething = true;
-                    totalOrderInY.add(neighbour);
+                    totalOrderInY.add(0, neighbour);
                     for (int neighbour2 : outDegreeY.keySet()) {
                         if (graphWithoutX.get(neighbour2).contains(neighbour)) {
                             outDegreeY.put(neighbour2, outDegreeY.get(neighbour2) - 1);
@@ -137,7 +209,7 @@ public class FeedbackVertexSet {
     }
 
     private HashMap<Integer, Set<Integer>> removeNodesFromGraph(HashMap<Integer, Set<Integer>> kPlusOneGraph, ArrayList<Integer> x) {
-        HashMap<Integer, Set<Integer>> newGraph = new HashMap<Integer, Set<Integer>>(kPlusOneGraph);
+        HashMap<Integer, Set<Integer>> newGraph = new HashMap<>(kPlusOneGraph);
         for (int node : x) {
             newGraph.remove(node);
         }
